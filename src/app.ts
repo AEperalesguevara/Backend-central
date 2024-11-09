44cac85a3b2c62538ff4a1b4b76373fd26b01c8c
Augusto <augusto.perales.g@uni.pe>
2024-11-09 17:41:31 -0500

    all

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import foodRouter from "../routes/foodRoute";
+import userRouter from "../routes/userRoute";
+import "dotenv/config";
+import orderRouter from "../routes/orderRoute";
+import cartRouter from "../routes/cartRoute";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/order",orderRouter);
+app.use("/uploads", express.static("uploads"));
+app.use("/api/cart", cartRouter);
+export default app;
