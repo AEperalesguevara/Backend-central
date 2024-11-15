ef6ef0313d5f55adf8ff5a12ae15bdc8e8b7486c
Augusto <augusto.perales.g@uni.pe>
2024-11-15 15:41:11 -0500

    new dist

--- dist/src/app.js
+++ dist/src/app.js
@@ -11,13 +11,20 @@ require("dotenv/config");
 const orderRoute_1 = __importDefault(require("../routes/orderRoute"));
 const cartRoute_1 = __importDefault(require("../routes/cartRoute"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+// Configuración de CORS
+app.use((0, cors_1.default)({
+    origin: "*", // Reemplaza con el dominio de tu frontend
+    methods: "GET, POST, PUT, DELETE", // Métodos HTTP permitidos
+    allowedHeaders: "Content-Type, Authorization", // Encabezados permitidos
+    credentials: true, // Permite el uso de cookies y autenticación en las solicitudes
+}));
+// Middleware para parsear el cuerpo de las solicitudes como JSON
 app.use(express_1.default.json());
-// Definir rutas
+// Rutas para las diferentes secciones de la API
 app.use("/api/food", foodRoute_1.default);
 app.use("/api/user", userRoute_1.default);
 app.use("/api/order", orderRoute_1.default);
 app.use("/api/cart", cartRoute_1.default);
-// Definir ruta estática para imágenes
-app.use("/images", express_1.default.static("uploads"));
+// Ruta estática para servir las imágenes
+app.use("/uploads", express_1.default.static("uploads"));
 exports.default = app;
